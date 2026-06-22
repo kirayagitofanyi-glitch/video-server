9f1c4490c4f32313390dd8eb356a6eacffbb70a2
kirayagitofanyi-glitch <kirayagitofanyi@gmail.com>
2026-06-22 11:08:31 +0200

    Create trending.js

---
+++ trending.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  res.json({
+    success: true,
+    videos: []
+  });
+});
+
+module.exports = router;
