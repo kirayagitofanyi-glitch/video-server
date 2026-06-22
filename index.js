049c388cca005c04cfb8865fff5d67fcc64e18bc
kirayagitofanyi-glitch <kirayagitofanyi@gmail.com>
2026-06-22 11:51:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const routes = require("./routes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "Video Server Running"
-  });
-});
+app.use("/", routes);
 
 const PORT = process.env.PORT || 3000;
 
